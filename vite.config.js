aafbf729a8d11a5be88dd3b9e9209859f8d2c4f6
Lzzs <147058959+lzzsG@users.noreply.github.com>
2025-03-25 22:01:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/vite-template/",
   plugins: [react()],
   define: {
     // 提供 global 的 polyfill
@@ -17,4 +18,4 @@ export default defineConfig({
   optimizeDeps: {
     include: ["buffer"],
   },
-})
+});
